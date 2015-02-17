e22dabcf860c8e95b9950ccaed971e475c34af5c
steveoh <sgourley@utah.gov>
2015-02-17 09:59:59 -0700

    add jshint stylish
    closes #99

--- GruntFile.js
+++ GruntFile.js
@@ -217,6 +217,7 @@ module.exports = function(grunt) {
                 src: jshintFiles
             },
             options: {
+                reporter: require('jshint-stylish'),
                 jshintrc: '.jshintrc'
             }
         },
