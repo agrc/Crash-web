3759aa07bab4d5c57d8eec03874a0f41c5616828
steveoh <sgourley@utah.gov>
2015-03-26 14:07:53 -0600

    Release v0.10.0

--- src/app/config.js
+++ src/app/config.js
@@ -14,7 +14,7 @@ define(['dojo/has', 'esri/config'], function(has, esriConfig) {
 
         // version.: String
         //      The version number.
-        version: '0.9.1',
+        version: '0.10.0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
