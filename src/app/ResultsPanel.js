b6f1531cced23805fafc938af1891d242479577d
steveoh <sgourley@utah.gov>
2015-04-21 18:08:44 -0600

    fix query for stage db
    update style
    refs #17
    fix jquery dependency

--- src/app/ResultsPanel.js
+++ src/app/ResultsPanel.js
@@ -168,7 +168,6 @@ define([
                     },
                     title: {
                         text: '',
-
                         style: {
                             display: 'none'
                         }
