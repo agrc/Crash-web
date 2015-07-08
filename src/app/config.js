8eb731bd64e1633adae2151082c95972ceea5323
steveoh <sgourley@utah.gov>
2015-07-08 14:46:42 -0600

    Release v0.17.1

--- src/app/config.js
+++ src/app/config.js
@@ -15,7 +15,7 @@ define(['dojo/has', 'esri/config'], function (has, esriConfig) {
 
         // version.: String
         //      The version number.
-        version: '0.17.0',
+        version: '0.17.1',
 
         // minDate: String
         // summary:
