2a5fe564db5fdb268138eb9c3f5e354ed2c4a0a4
steveoh <sgourley@utah.gov>
2016-07-26 16:29:20 -0600

    update for mercator points

--- scripts/create_points_json.py
+++ scripts/create_points_json.py
@@ -16,9 +16,9 @@ table = os.path.join(script_dir, '{}.sde'.format(configuration), 'DDACTS.DDACTSa
 with open(file, 'w') as f:
     with SearchCursor(table, ['OID@', 'SHAPE@XY']) as cursor:
         for row in cursor:
-            if row[1][0] > 0 and row[1][1] > 0:
-                x = round(row[1][0], 2)
-                y = round(row[1][1], 2)
+            if row[1][0] is not None and row[1][1] is not None:
+                x = int(row[1][0])
+                y = int(row[1][1])
 
                 point = [row[0], x, y]
                 dict['points'].append(point)
