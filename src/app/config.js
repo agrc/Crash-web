8fa8de5c525ce5f95403694a6f28a650726482ae
steveoh <sgourley@utah.gov>
2016-08-02 18:54:32 -0600

    Release v2.0.0

--- src/app/config.js
+++ src/app/config.js
@@ -25,7 +25,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.0.3',
+        version: '2.0.0',
 
         // minDate: String
         // summary:
