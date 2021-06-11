c9d0e0d42f9817ad2144291182ea2efb57f6a771
steveoh <sgourley@utah.gov>
2021-06-10 20:50:36 -0600

    chore: add project for cloud functions

---
+++ api/chart-function/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace chart_function.Models
+{
+
+    public class Result
+    {
+        // [JsonProperty(PropertyName = "weather")]
+        public Chart Weather { get; set; }
+
+        // [JsonProperty(PropertyName = "road")]
+        public Chart Road { get; set; }
+
+        // [JsonProperty(PropertyName = "cause")]
+        public Chart Cause { get; set; }
+
+        // [JsonProperty(PropertyName = "distraction")]
+        public Chart Distractions { get; set; }
+
+        // [JsonProperty(PropertyName = "day")]
+        public Chart Days { get; set; }
+
+        // [JsonProperty(PropertyName = "time")]
+        public Chart Time { get; set; }
+    }
+}
